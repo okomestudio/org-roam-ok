d3f842c7ead8ce5ccbb78d823a6bd02a3d63f229
Taro Sato <okomestudio@gmail.com>
2024-08-12 15:35:27 -0700

    Initial commit

---
+++ org-roam-plugin-ja.el
@@ -0,0 +1,11 @@
+;;; org-roam-plugin-ja.el --- Org Roam Plugin for Japanese  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; This is the top-level module. Load all plugin modules here.
+;;
+;;; Code:
+
+(require 'org-roam-plugin-ja-unlinked-references)
+
+(provide 'org-roam-plugin-ja)
+;;; org-roam-plugin-ja.el ends here
