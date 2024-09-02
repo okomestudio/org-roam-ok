d5587585645f3730e4e529a071b1562a621aad29
Taro Sato <okomestudio@gmail.com>
2024-09-02 12:51:19 -0700

    Make name prefix consistent

--- orp-ok-node.el
+++ orp-ok-node.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/okomestudio/org-roam-plugin-ok
 ;; Version: 0.1
 ;; Keywords: org-mode, org-roam, plug-in
-;; Package-Requires: ((emacs "29.1") (org "9.7") (org-roam "2.2.2"))
+;; Package-Requires: ((emacs "29.1") (org "9.7") (org-roam "2.2.2") (marginalia "1.6"))
 ;;
 ;;; Commentary:
 ;;
@@ -16,9 +16,10 @@
 
 (require 'orp-ok-utils)
 (require 'org-roam-timestamps)
+(require 'marginalia)
 
 (with-eval-after-load 'org-roam-node
-  (defun orp-visit-parent-of-node-at-point (node)
+  (defun orp-ok-node-visit-parent-of-node-at-point (node)
     "Visit parent of given NODE at point, if exists."
     (interactive "P")
     (let* ((node (or node (org-roam-node-at-point)))
@@ -29,33 +30,33 @@
         (message "No parent found for this node"))))
 
   ;; Node title rendering utility functions.
-  (defvar orp--file-node-cache '()
+  (defvar orp-ok-node--file-node-cache '()
     "Cache for file nodes.")
 
-  (defun orp--file-node-cache-maybe-invalidate ()
+  (defun orp-ok-node--file-node-cache-maybe-invalidate ()
     "Invalidate file node cache if the saved buffer is Org file."
     (let ((file buffer-file-name))
       (when (string= (file-name-extension file) "org")
-        (setf orp--file-node-cache (assoc-delete-all file orp--file-node-cache)))))
+        (setf orp-ok-node--file-node-cache (assoc-delete-all file orp-ok-node--file-node-cache)))))
 
-  (add-hook 'after-save-hook #'orp--file-node-cache-maybe-invalidate)
+  (add-hook 'after-save-hook #'orp-ok-node--file-node-cache-maybe-invalidate)
 
-  (defun orp--node-id-from-file (file)
+  (defun orp-ok-node--id-from-file (file)
     "Get the node ID for FILE."
     (caar (org-roam-db-query `[:select :distinct nodes:id :from nodes
                                        :where (and (= nodes:file ,file)
                                                    (= nodes:level 0))])))
 
-  (defun orp--node-from-file (file)
+  (defun orp-ok-node--from-file (file)
     "Get the node for FILE. Use the cache whenever possible."
-    (let ((cached (assoc file orp--file-node-cache)))
+    (let ((cached (assoc file orp-ok-node--file-node-cache)))
       (if cached
           (cdr (cdr cached))
-        (let ((node (org-roam-node-from-id (orp--node-id-from-file file))))
-          (push `(,file . (,(float-time) . ,node)) orp--file-node-cache)
+        (let ((node (org-roam-node-from-id (orp-ok-node--id-from-file file))))
+          (push `(,file . (,(float-time) . ,node)) orp-ok-node--file-node-cache)
           node))))
 
-  (defun orp--parent-title-get (node)
+  (defun orp-ok-node--parent-title-get (node)
     "Get the title of the NODE's parent."
     (let ((parent (cdr (assoc-string "PARENT" (org-roam-node-properties node)))))
       (when parent
@@ -66,22 +67,22 @@
                                   "\\2"
                                   parent))))
 
-  (defun orp--title-aux-get (node)
+  (defun orp-ok-node--title-aux-get (node)
     "Get the auxiliary title info for NODE."
     (let ((node-title (org-roam-node-title node)))
       (if (member node-title (org-roam-node-aliases node))
           (list " = " (org-roam-node-file-title node))
         (if (eq 0 (org-roam-node-level node))
             ;; File-level node
-            (let ((pt (orp--parent-title-get node)))
+            (let ((pt (orp-ok-node--parent-title-get node)))
               (if pt
                   (list " ❬ " pt)))
           ;; Non-file-level node
-          (let* ((p (orp--node-from-file (org-roam-node-file node)))
-                 (pt (orp--parent-title-get p)))
+          (let* ((p (orp-ok-node--from-file (org-roam-node-file node)))
+                 (pt (orp-ok-node--parent-title-get p)))
             (list " ❬ " (or pt (org-roam-node-file-title node))))))))
 
-  (defun orp--title-aux-render (title-aux)
+  (defun orp-ok-node--title-aux-render (title-aux)
     (if (not title-aux)
         ""
       (let ((sym (nth 0 title-aux)) ;; symbol
@@ -94,9 +95,11 @@
         (concat (propertize sym 'face face-sym)
                 (propertize aux 'face face-aux)))))
 
+  ;; Public accessor methods
+
   (cl-defmethod org-roam-node-orp-title ((node org-roam-node))
     (concat (org-roam-node-title node)
-            (orp--title-aux-render (orp--title-aux-get node))))
+            (orp-ok-node--title-aux-render (orp-ok-node--title-aux-get node))))
 
   (cl-defmethod org-roam-node-orp-tags ((node org-roam-node))
     (let ((tags (if (eq 0 (org-roam-node-level node))
@@ -118,7 +121,6 @@
       (format-time-string "%Y-%m-%d" mtime)))
 
   (cl-defmethod org-roam-node-orp-timestamp-marginalia ((node org-roam-node))
-    (require 'marginalia)
     (let* ((inhibit-message t)
            (mtime (cdr (assoc "MTIME" (org-roam-node-properties node))))
            (mtime (if mtime
