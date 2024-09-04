77ee705d21a243d74509aada8da15049c5d589a3
Taro Sato <okomestudio@gmail.com>
2024-09-04 12:30:56 -0700

    Use shorthands

--- orp-ok-node.el
+++ orp-ok-node.el
@@ -20,26 +20,26 @@
 
 (with-eval-after-load 'org-roam-node
   ;;; The org-roam cache layer (sqlite)
-  (defun orp-ok-node--all-node-ids-within-file (file)
+  (defun oon--all-node-ids-within-file (file)
     "Get the IDs of all nodes within FILE."
     (mapcar 'car
             (org-roam-db-query `[:select :distinct nodes:id
                                          :from nodes
                                          :where (and (= nodes:file ,file))])))
 
-  (defun orp-ok-node--file-node-id (node)
+  (defun oon--file-node-id (node)
     "Get the ID of the top-level file node of NODE."
     (let ((file (org-roam-node-file node)))
       (caar (org-roam-db-query `[:select nodes:id :from nodes
                                          :where (and (= nodes:file ,file)
                                                      (= nodes:level 0))]))))
 
-  (defun orp-ok-node--all-file-nodes-and-ids ()
+  (defun oon--all-file-nodes-and-ids ()
     (org-roam-db-query '[:select [nodes:file nodes:id]
                                  :from nodes
                                  :where (= nodes:level 0)]))
 
-  (defun orp-ok-node--all-nodes ()
+  (defun oon--all-nodes ()
     (org-roam-db-query '[:select nodes:id :from nodes]))
 
 ;;; In-memory cache
@@ -48,40 +48,40 @@
   ;;
   ;; TODO: Make the implementation memory efficient.
 
-  (defvar orp-ok-node--cache-in-memory-file (make-hash-table :test 'equal)
+  (defvar oon--cache-in-memory-file (make-hash-table :test 'equal)
     "In-memory cache, mapping a file to the ID of its top-level node.")
 
-  (defun orp-ok-node--cache-in-memory-file-get (file)
-    (gethash file orp-ok-node--cache-in-memory-file))
+  (defun oon--cache-in-memory-file-get (file)
+    (gethash file oon--cache-in-memory-file))
 
-  (defun orp-ok-node--cache-in-memory-file-save (file node-id)
-    (puthash file node-id orp-ok-node--cache-in-memory-file))
+  (defun oon--cache-in-memory-file-save (file node-id)
+    (puthash file node-id oon--cache-in-memory-file))
 
-  (defun orp-ok-node--cache-in-memory-file-remove (file)
-    (remhash file orp-ok-node--cache-in-memory-file))
+  (defun oon--cache-in-memory-file-remove (file)
+    (remhash file oon--cache-in-memory-file))
 
-  (defun orp-ok-node--cache-in-memory-file-fill ()
-    (dolist (row (orp-ok-node--all-file-nodes-and-ids))
-      (orp-ok-node--cache-in-memory-file-save (car row) (cadr row))))
+  (defun oon--cache-in-memory-file-fill ()
+    (dolist (row (oon--all-file-nodes-and-ids))
+      (oon--cache-in-memory-file-save (car row) (cadr row))))
 
-  (defvar orp-ok-node--cache-in-memory (make-hash-table :test 'equal)
+  (defvar oon--cache-in-memory (make-hash-table :test 'equal)
     "In-memory cache, mapping a node ID to its node.")
 
-  (defun orp-ok-node--cache-in-memory-get (node-id)
+  (defun oon--cache-in-memory-get (node-id)
     "Get the node for NODE-ID from the in-memory cache."
-    (cdr (gethash node-id orp-ok-node--cache-in-memory)))
+    (cdr (gethash node-id oon--cache-in-memory)))
 
-  (defun orp-ok-node--cache-in-memory-save (node)
+  (defun oon--cache-in-memory-save (node)
     "Save NODE to the in-memory cache."
     (let* ((node-id (org-roam-node-id node))
            (level (org-roam-node-level node))
            (file-node-id (when (< 0 level)
                            (let* ((file (org-roam-node-file node))
-                                  (id (orp-ok-node--cache-in-memory-file-get file)))
+                                  (id (oon--cache-in-memory-file-get file)))
                              (if id
                                  id
-                               (setq id (orp-ok-node--file-node-id node))
-                               (orp-ok-node--cache-in-memory-file-save file id)
+                               (setq id (oon--file-node-id node))
+                               (oon--cache-in-memory-file-save file id)
                                id))))
            (file-parent-node-id
             (when (= 0 level)
@@ -93,48 +93,48 @@
                    "\\1"
                    parent)))))
            (item `(,node ,file-node-id ,file-parent-node-id)))
-      (puthash node-id `(,(float-time) ,@item) orp-ok-node--cache-in-memory)
+      (puthash node-id `(,(float-time) ,@item) oon--cache-in-memory)
       item))
 
-  (defun orp-ok-node--cache-in-memory-remove (node-id)
+  (defun oon--cache-in-memory-remove (node-id)
     "Save NODE to the in-memory cache."
-    (remhash node-id orp-ok-node--cache-in-memory))
+    (remhash node-id oon--cache-in-memory))
 
-  (defun orp-ok-node--cache-in-memory-fill ()
-    (dolist (row (orp-ok-node--all-nodes))
-      (orp-ok-node--from-id (car row))))
+  (defun oon--cache-in-memory-fill ()
+    (dolist (row (oon--all-nodes))
+      (oon--from-id (car row))))
 
-  (defun orp-ok-node--cache-in-memory-maybe-remove ()
+  (defun oon--cache-in-memory-maybe-remove ()
     (let ((file buffer-file-name))
       (when (string= (file-name-extension file) "org")
-        (orp-ok-node--cache-in-memory-file-remove file)
-        (dolist (node-id (orp-ok-node--all-node-ids-within-file file))
-          (orp-ok-node--cache-in-memory-remove node-id)))))
+        (oon--cache-in-memory-file-remove file)
+        (dolist (node-id (oon--all-node-ids-within-file file))
+          (oon--cache-in-memory-remove node-id)))))
 
-  (add-hook 'after-save-hook #'orp-ok-node--cache-in-memory-maybe-remove)
+  (add-hook 'after-save-hook #'oon--cache-in-memory-maybe-remove)
 
   ;;; Node utility functions
 
-  (defun orp-ok-node--from-id (node-id)
+  (defun oon--from-id (node-id)
     "Get the node with NODE-ID either from in-memory cache or Org cache."
-    (let ((node (car (orp-ok-node--cache-in-memory-get node-id))))
+    (let ((node (car (oon--cache-in-memory-get node-id))))
       (if node
           node
         (setq node (org-roam-node-from-id node-id))
-        (orp-ok-node--cache-in-memory-save node)
+        (oon--cache-in-memory-save node)
         node)))
 
-  (defun orp-ok-node--file-node-from-id (node-id)
-    (let ((node-and-parent-id (orp-ok-node--cache-in-memory-get node-id)))
+  (defun oon--file-node-from-id (node-id)
+    (let ((node-and-parent-id (oon--cache-in-memory-get node-id)))
       (when (not node-and-parent-id)
-        (setq node-and-parent-id (orp-ok-node--cache-in-memory-save (org-roam-node-from-id node-id))))
+        (setq node-and-parent-id (oon--cache-in-memory-save (org-roam-node-from-id node-id))))
       (let ((parent-node-id (cadr node-and-parent-id)))
         (when parent-node-id
-          (orp-ok-node--from-id parent-node-id)))))
+          (oon--from-id parent-node-id)))))
 
-  (defun orp-ok-node--parent-titles (node)
+  (defun oon--parent-titles (node)
     "Get the title of the file NODE's parent."
-    (let* ((parent-id (caddr (orp-ok-node--cache-in-memory-get (org-roam-node-id node))))
+    (let* ((parent-id (caddr (oon--cache-in-memory-get (org-roam-node-id node))))
            (visited nil)
            titles)
       (catch 'circular
@@ -143,36 +143,36 @@
               (progn
                 (message "orp-ok-node: Circular nodes detected!")
                 (throw 'circular nil)))
-          (setq node (orp-ok-node--from-id parent-id))
+          (setq node (oon--from-id parent-id))
           (setq titles (append titles `(,(org-roam-node-title node))))
           (setq visited (append visited `(,parent-id)))
-          (setq parent-id (caddr (orp-ok-node--cache-in-memory-get parent-id))))
+          (setq parent-id (caddr (oon--cache-in-memory-get parent-id))))
         titles)))
 
-  (defun orp-ok-node--title-aux-get (node)
+  (defun oon--title-aux-get (node)
     "Get the auxiliary title info for NODE."
     (let ((title-or-alias (org-roam-node-title node)))
       (if (member title-or-alias (org-roam-node-aliases node))
           ;; `node' may have its title replaced with an alias, so pull
           ;; the title from the original node:
-          (list " = " (org-roam-node-title (orp-ok-node--from-id (org-roam-node-id node))))
+          (list " = " (org-roam-node-title (oon--from-id (org-roam-node-id node))))
         (let ((section-delimiter " ❬ ")
               sections)
           (setq sections
                 (if (eq 0 (org-roam-node-level node))
                     ;; File-level node
-                    (let ((parent-titles (orp-ok-node--parent-titles node)))
+                    (let ((parent-titles (oon--parent-titles node)))
                       (if parent-titles
                           parent-titles))
                   ;; Non-file-level node
-                  (let* ((p (orp-ok-node--file-node-from-id (org-roam-node-id node)))
-                         (parent-titles (orp-ok-node--parent-titles p)))
+                  (let* ((p (oon--file-node-from-id (org-roam-node-id node)))
+                         (parent-titles (oon--parent-titles p)))
                     (or parent-titles
                         `(,(org-roam-node-file-title node))))))
           (flatten-list (mapcar (lambda (x) `(,section-delimiter ,x))
                                 sections))))))
 
-  (defun orp-ok-node--title-aux-render (title-aux)
+  (defun oon--title-aux-render (title-aux)
     (if (not title-aux)
         ""
       (let ((face-sym `(:foreground ,(face-attribute 'completions-annotations
@@ -196,7 +196,7 @@
 
   (cl-defmethod org-roam-node-orp-title ((node org-roam-node))
     (concat (org-roam-node-title node)
-            (orp-ok-node--title-aux-render (orp-ok-node--title-aux-get node))))
+            (oon--title-aux-render (oon--title-aux-get node))))
 
   (cl-defmethod org-roam-node-orp-tags ((node org-roam-node))
     (let ((tags (if (eq 0 (org-roam-node-level node))
@@ -231,7 +231,7 @@
 
   ;; Interactive functions
 
-  (defun orp-ok-node-visit-parent-of-node-at-point (node)
+  (defun oon-visit-parent-of-node-at-point (node)
     "Visit parent of given NODE at point, if exists."
     (interactive "P")
     (let* ((node (or node (org-roam-node-at-point)))
@@ -243,12 +243,18 @@
 
   ;;; Fill cache on idle for speeding up the first invocation
 
-  (defun orp-ok-node--on-idle ()
+  (defun oon--on-idle ()
+    "Fill cache on idle."
     (message "On idle...")
-    (orp-ok-node--cache-in-memory-fill)
-    (orp-ok-node--cache-in-memory-file-fill))
+    (oon--cache-in-memory-fill)
+    (oon--cache-in-memory-file-fill))
 
-  (run-with-idle-timer 10 nil #'orp-ok-node--on-idle))
+  (run-with-idle-timer 10 nil #'oon--on-idle))
+
 
 (provide 'orp-ok-node)
+
+;; Local Variables:
+;; read-symbol-shorthands: (("oon" . "orp-ok-node"))
+;; End:
 ;;; orp-ok-node.el ends here
