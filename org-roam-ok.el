af9998e0a336c16e1e27a2d23f6c3529685cc2b6
Taro Sato <okomestudio@gmail.com>
2025-03-01 22:57:12 -0800

    Use async for on-idle cache initialization

--- org-roam-ok.el
+++ org-roam-ok.el
@@ -4,9 +4,9 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/org-roam-ok
-;; Version: 0.3
+;; Version: 0.4
 ;; Keywords: org-mode, roam, plug-in
-;; Package-Requires: ((emacs "30.1") (org "9.7") (org-roam "20250111.252") (adaptive-wrap "0.8") (dash "2.13") (marginalia "1.6") (ok-plural "0.1") (org-roam-timestamps "1.0.0") (s "1.13.1"))
+;; Package-Requires: ((emacs "30.1") (org "9.7") (org-roam "20250111.252") (adaptive-wrap "0.8") (async "1.9.7") (dash "2.13") (marginalia "1.6") (ok "0.2.1") (ok-plural "0.1") (org-roam-timestamps "1.0.0") (s "1.13.1"))
 ;;
 ;;; License:
 ;;
@@ -30,12 +30,15 @@
 ;;
 ;;; Code:
 
+(require 'async)
+(require 'ok)
+
 (defgroup org-roam-ok nil
   "Group for `org-roam-ok'."
   :group 'org-roam-ok
   :prefix "org-roam-ok-")
 
-(defvar org-roam-ok-version "0.3"
+(defvar org-roam-ok-version "0.4"
   "Package version.")
 
 (defcustom org-roam-ok-on-idle-delay 60
@@ -51,16 +54,40 @@
   "Initialize `org-roam' on idle."
   (when org-roam-ok--on-idle-timer
     (cancel-timer org-roam-ok--on-idle-timer))
+
   (remove-hook 'post-command-hook #'org-roam-ok--on-idle-init-scheduler)
+
   (when (not (and (boundp 'org-roam-ok-node--cache-in-memory)
                   (> (hash-table-count org-roam-ok-node--cache-in-memory) 0)))
-    (message "Starting org-roam-init-on-idle...")
-    (require 'org-roam)
-    (org-roam-node-list)
 
-    (org-roam-ok-mode 1)
-    (org-roam-ok-node-project-org-file--load #'org-roam-ok-node-fill-caches)
-    (message "Finished org-roam-init-on-idle")))
+    (when (or (not (boundp 'org-roam-ok-node-fill-caches--lock))
+              (not org-roam-ok-node-fill-caches--lock))
+      (setq org-roam-ok-node-fill-caches--lock t)
+      (async-start
+       `(lambda ()
+          (let ((load-path '(,@load-path)))
+            (require 'org-roam)
+            (require 'org-roam-ok)
+            (org-roam-ok-enhance)
+
+            (let ((org-roam-ok-node-project-org-file ,org-roam-ok-node-project-org-file))
+              (org-roam-ok-node-project-org-file--load #'org-roam-ok-node-fill-caches))
+
+            `(,org-roam-ok-node--cache-in-memory
+              ,org-roam-ok-node--cache-in-memory-file)))
+
+       (lambda (result)
+         (let* ((cache-in-memory (car result))
+                (cache-in-memory-file (cadr result)))
+           (message "Filled in-memory cache with %d nodes in %d files"
+                    (hash-table-size cache-in-memory)
+                    (hash-table-size cache-in-memory-file))
+           (setq org-roam-ok-node--cache-in-memory cache-in-memory
+                 org-roam-ok-node--cache-in-memory-file cache-in-memory-file
+                 org-roam-ok-node-fill-caches--lock nil)))))))
+
+(advice-add #'org-roam-ok--init-cache :around #'ok-debug-ad-function-beg-end)
+(advice-add #'org-roam-ok--init-on-idle :around #'ok-debug-ad-function-beg-end)
 
 (defun org-roam-ok--on-idle-init-scheduler ()
   "Schedule on-idle initializer."
@@ -76,6 +103,19 @@
   "Set up on-idle initializer."
   (org-roam-ok--on-idle-init-scheduler)
   (add-hook 'post-command-hook #'org-roam-ok--on-idle-init-scheduler))
+
+;;; Public functions
+
+;;;###autoload
+(defun org-roam-ok-enhance ()
+  "Enhance `org-roam' with `org-roam-ok'."
+  (setopt find-file-visit-truename t)  ; see "5.3 Setting up Org-roam"
+  (require 'org-roam-dailies)
+  (require 'org-roam-gt)
+  (org-roam-gt-mode 1)
+  (org-roam-ok-mode 1))
+
+;;; Minor mode config
 
 (defun org-roam-ok-activate ()
   "Activate `org-roam-ok-mode'."
