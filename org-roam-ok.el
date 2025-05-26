fbe9b8a1aa9ff74a7470ce7f00a0fb3fcb920323
Taro Sato <okomestudio@gmail.com>
2025-05-25 21:37:49 -0700

    Use the function version of org-roam-node-display-template

--- org-roam-ok.el
+++ org-roam-ok.el
@@ -4,9 +4,9 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/org-roam-ok
-;; Version: 0.4.3
+;; Version: 0.5.1
 ;; Keywords: org-mode, roam, plug-in
-;; Package-Requires: ((emacs "30.1") (org "9.7") (org-roam "20250111.252") (adaptive-wrap "0.8") (async "1.9.7") (dash "2.19.1") (marginalia "1.6") (ok "0.2.3") (ok-plural "0.1") (org-ref "20250301.1918") (org-roam-timestamps "1.0.0") (s "1.13.1"))
+;; Package-Requires: ((emacs "30.1") (org "9.7") (org-roam "20250525.715") (adaptive-wrap "0.8") (async "1.9.7") (dash "2.19.1") (marginalia "1.6") (ok "0.2.3") (ok-plural "0.1") (org-ref "20250301.1918") (org-roam-timestamps "1.0.0") (s "1.13.1"))
 ;;
 ;;; License:
 ;;
@@ -37,7 +37,7 @@
   :group 'org-roam-ok
   :prefix "org-roam-ok-")
 
-(defvar org-roam-ok-version "0.4.2"
+(defvar org-roam-ok-version "0.5.1"
   "Package version.")
 
 (defcustom org-roam-ok-on-idle-delay 60
@@ -84,8 +84,6 @@ When set to nil, the on-idle initializer will not run."
   "Enhance `org-roam' with `org-roam-ok'."
   (setopt find-file-visit-truename t)  ; see "5.3 Setting up Org-roam"
   (require 'org-roam-dailies)
-  (require 'org-roam-gt)
-  (org-roam-gt-mode 1)
   (org-roam-ok-mode 1))
 
 ;;; Minor mode config
@@ -96,9 +94,12 @@ When set to nil, the on-idle initializer will not run."
   (require 'org-roam-ok-capture)
   (require 'org-roam-ok-mode)
   (require 'org-roam-ok-node)
-  (with-eval-after-load 'org-roam-gt
-    (require 'org-roam-ok-node-gt))
   (require 'org-roam-ok-ja)
+
+  (add-hook 'after-save-hook #'oron-display--cache-maybe-remove)
+  (advice-add #'org-roam-ok-node-display-template :around
+              #'org-roam-ok-node-display-cache-rendered)
+  (setopt org-roam-node-display-template #'org-roam-ok-node-display-template)
 
   ;; Wrap these functions so that each will load the project
   ;; .dir-locals.el before its execution.
@@ -109,7 +110,7 @@ When set to nil, the on-idle initializer will not run."
 
 (defun org-roam-ok-deactivate ()
   "Deactivate `org-roam-ok-mode'."
-  nil)
+  (remove-hook 'after-save-hook #'oron-display--cache-maybe-remove))
 
 ;;;###autoload
 (define-minor-mode org-roam-ok-mode
