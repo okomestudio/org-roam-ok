7b2eb16c21c8095e6a87111bca8565958c3e3aa8
Taro Sato <okomestudio@gmail.com>
2026-02-12 13:08:19 -0800

    Wrap lower-level API functions

--- org-roam-ok.el
+++ org-roam-ok.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/org-roam-ok
-;; Version: 0.14.8
+;; Version: 0.14.9
 
 ;; Keywords: org-mode, roam, plug-in
 ;; Package-Requires: ((emacs "30.1") (org "9.7") (org-ok "0.5.1") (org-roam "2.3.1") (async "1.9.7") (dash "2.20") (marginalia "1.6") (ok "0.12.2") (org-ref "3.1") (org-roam-timestamps "1.0.0") (s "1.13.1"))
@@ -38,7 +38,7 @@
   :group 'org-roam-ok
   :prefix "org-roam-ok-")
 
-(defvar org-roam-ok-version "0.14.8"
+(defvar org-roam-ok-version "0.14.9"
   "Package version.")
 
 (defcustom org-roam-ok-on-idle-delay 60
@@ -98,8 +98,8 @@ When set to nil, the on-idle initializer will not run."
 
   ;; Wrap these functions so that each will load the project .dir-locals.el
   ;; before its execution.
-  (dolist (func '(org-roam-node-find
-                  org-roam-node-insert
+  (dolist (func '(org-roam-node-list
+                  org-roam-node-read
                   org-roam-dailies-capture-today))
     (advice-add func :around #'org-roam-ok-node-project-org-file--load)))
 
